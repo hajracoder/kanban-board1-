3bdf607436d6a648ef80e94e3caf8a4dc0194106
hajracoder <hajraqadir94@gmail.com>
2025-08-03 06:44:23 -0600

    Saving all changes before rebase

--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,4 @@
-// export type TaskStatus = "to-do" | "progress" | "done";
 
-// export type Task = {
-//   id: string;
-//   title: string;
-//   description?: string;
-//   date?: string;
-//   status: TaskStatus;
-//   avatar?: string;
-//   github?: string;
-//   linkedin?: string;
-//   facebook?: string;
-  
-// };
 
 export type TaskStatus = "to-do" | "progress" | "done";
 
