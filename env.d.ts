946682cc5ca5b9c510e1e97b5bf2a37e80b34e25
hajracoder <hajraqadir94@gmail.com>
2025-08-11 05:49:42 -0600

    Fix: Added node types for process.env and installed @types/node

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly REACT_APPWRITE_DATABASE_ID: string;
+  readonly REACT_APPWRITE_COLLECTION_ID_USERS: string;
+  // add other env variables here as needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
